3a539a3a5c79620fda3f2c60660226b9861b80d3
Birkity <85987355+Birkity@users.noreply.github.com>
2024-12-23 19:46:34 +0300

    Add Transaction class with id, title, amount, and date properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,76 @@
+import 'package:expense_planner/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyHomePage());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "t1",
+      title: "New Shoes",
+      amount: 70.1,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter App'),
-        ),
-        body: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART'),
-                elevation: 5,
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: const Card(
+              color: Colors.blue,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'CHART',
+                  style: TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
               ),
+              elevation: 5,
             ),
-          ],
-        ));
+          ),
+          Container(
+            width: double.infinity,
+            child: const Card(
+              color: Colors.blue,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'LIST OF TX',
+                  style: TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              elevation: 5,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
