702ac3287ab3e3addddf0ed0bc271c9aa90c19c4
Birkity <85987355+Birkity@users.noreply.github.com>
2024-12-24 11:41:20 +0300

    Add method to add new transactions

--- lib/widgets/user_transaction.dart
+++ lib/widgets/user_transaction.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:expense_planner/widgets/new_transaction.dart';
 import 'package:expense_planner/widgets/transaction_list.dart';
+import 'package:expense_planner/models/transaction.dart';
 
 class UserTransaction extends StatefulWidget {
   const UserTransaction({super.key});
@@ -10,12 +11,33 @@ class UserTransaction extends StatefulWidget {
 }
 
 class _UserTransactionState extends State<UserTransaction> {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "t1",
+      title: "New Shoes",
+      amount: 70.1,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+        id: DateTime.now().millisecond.toString(),
+        title: title,
+        amount: amount,
+        date: DateTime.now());
+
+    setState(() {
+      transactions.add(newTx);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         NewTransaction(),
-        TransactionList(),
+        TransactionList(transactions),
       ],
     );
   }
